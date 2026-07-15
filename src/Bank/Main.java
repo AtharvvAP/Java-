8813de56df7d064373a308363cefdc8439d74086
Atharvvap <atharva.ap.04@gmail.com>
2026-07-15 21:26:47 +0530

    started with exception handling

--- src/Bank/Main.java
+++ src/Bank/Main.java
@@ -3,103 +3,102 @@ package Bank;
 import java.util.*;
 import java.util.ArrayList;
 
-class Account{
-    String name;
-    long accountNo;
-    double balance;
-
-    Account(String n, long accNo, double bal){
-        name=n;
-        accountNo=accNo;
-        balance=bal;
-    }
-
-    void showDetails(){
-        System.out.println("Balance is " + balance);
-    }
-
-    public String toString(){
-        return name + " - " + accountNo + " - " + balance;
-    }
-}
-class savingAccount extends Account{
-    double interestRate;
-
-    savingAccount(String n, long accNo, double bal, double rate){
-        super(n,accNo,bal);
-        interestRate=rate;
-    }
-
-    void addInterest(){
-        double interestAmount = (balance * interestRate)/100;
-        balance = balance + interestAmount;
-        System.out.println("Available balance is " + interestAmount);
-    }
-}
-class sharesAccount extends Account{
-    int noOfShares;
-    int shPrice;
-
-    sharesAccount(String n, long accNo, double bal, int numSh, int shPri){
-        super(n,accNo,bal);
-        noOfShares=numSh;
-        shPrice=shPri;
-    }
-
-    void sharesCal(){
-        double amount = (noOfShares * shPrice);
-        System.out.println("Shares amount " + amount);
-    }
-}
-class divCal extends Account{
-    double sharesValue;
-    String memberType;
-
-    divCal(String n, long accNo, double bal, double shValue, String memType){
-        super(n,accNo,bal);
-        sharesValue=shValue;
-        memberType=memType;
-    }
-
-    void calDiv(){
-        double dividendAmount;
-        if(memberType.equals("Premium")){
-            dividendAmount=(sharesValue*8)/100;
-        } else if (memberType.equals("Regular")) {
-            dividendAmount=(sharesValue*5)/100;
-        }
-        else {
-            dividendAmount=0;
-        }
-        System.out.println("Dividend amount is " + dividendAmount);
-    }
-}
+//class Account{
+//    String name;
+//    long accountNo;
+//    double balance;
+//
+//    Account(String n, long accNo, double bal){
+//        name=n;
+//        accountNo=accNo;
+//        balance=bal;
+//    }
+//
+//    void showDetails(){
+//        System.out.println("Balance is " + balance);
+//    }
+//
+//    public String toString(){
+//        return name + " - " + accountNo + " - " + balance;
+//    }
+//}
+//class savingAccount extends Account{
+//    double interestRate;
+//
+//    savingAccount(String n, long accNo, double bal, double rate){
+//        super(n,accNo,bal);
+//        interestRate=rate;
+//    }
+//
+//    void addInterest(){
+//        double interestAmount = (balance * interestRate)/100;
+//        balance = balance + interestAmount;
+//        System.out.println("Available balance is " + interestAmount);
+//    }
+//}
+//class sharesAccount extends Account{
+//    int noOfShares;
+//    int shPrice;
+//
+//    sharesAccount(String n, long accNo, double bal, int numSh, int shPri){
+//        super(n,accNo,bal);
+//        noOfShares=numSh;
+//        shPrice=shPri;
+//    }
+//
+//    void sharesCal(){
+//        double amount = (noOfShares * shPrice);
+//        System.out.println("Shares amount " + amount);
+//    }
+//}
+//class divCal extends Account{
+//    double sharesValue;
+//    String memberType;
+//
+//    divCal(String n, long accNo, double bal, double shValue, String memType){
+//        super(n,accNo,bal);
+//        sharesValue=shValue;
+//        memberType=memType;
+//    }
+//
+//    void calDiv(){
+//        double dividendAmount;
+//        if(memberType.equals("Premium")){
+//            dividendAmount=(sharesValue*8)/100;
+//        } else if (memberType.equals("Regular")) {
+//            dividendAmount=(sharesValue*5)/100;
+//        }
+//        else {
+//            dividendAmount=0;
+//        }
+//        System.out.println("Dividend amount is " + dividendAmount);
+//    }
+//}
 public class Main{
     public static void main(String[] args) {
-        ArrayList<Account> accounts = new ArrayList<>();
-        HashMap<Long, Account> accountMap= new HashMap<>();
+        int balance = 8541;
 
-            savingAccount s1=new savingAccount("AAAAA", 123456789L, 4444454, 10);
-            sharesAccount sh1=new sharesAccount("BBBBB",147852369L,1200200,56,7500);
-            divCal d1=new divCal("CCCCC",321456789L, 74589612, 7500,"Premium");
+        Scanner sc= new Scanner(System.in);
 
-            accounts.add(s1);
-            accounts.add(sh1);
-            accounts.add(d1);
 
-        System.out.println(accounts);
+        try{
+            System.out.println("Enter amount for withdraw");
+            int amount = sc.nextInt();
 
-        accountMap.put((long) s1.accountNo , s1);
-        accountMap.put((long)sh1.accountNo , sh1);
-        accountMap.put((long) d1.accountNo , d1);
-
-        System.out.println("--------All accounts details----------");
-        for(Account acc : accounts){
-            acc.showDetails();
+            if(amount <= 0){
+                System.out.println("Invalid amount");
+            }
+            else if(amount <= balance){
+                balance = balance-amount;
+                System.out.println("Withdrawl success");
+                System.out.println("Remaining balance " + balance);
+            }else {
+                System.out.println("Insufficient balance");
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid Amount, Please enter valid numeric amount");
+        }finally {
+            System.out.println("Thank you for using");
         }
-
-        System.out.println("-------Lookup for account-------------");
-        Account found = accountMap.get(147852369L);
-        found.showDetails();
     }
 }
