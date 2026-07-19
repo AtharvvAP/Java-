9d2b26b7c9cb156b6f0a77cfc235ec3adcbf0072
Atharvvap <atharva.ap.04@gmail.com>
2026-07-19 20:39:56 +0530

    implement more than one functionality

--- src/Bank/Main.java
+++ src/Bank/Main.java
@@ -72,6 +72,29 @@ class sharesAccount extends Account{
         System.out.println("Shares Calculation " + shareCal);
     }
 }
+class divCal extends Account{
+    String memberType;
+    double sharesValue;
+
+    divCal(String n, long accno, double bal, String memType, double shValue){
+        super(n,accno,bal);
+        memberType=memType;
+        sharesValue=shValue;
+    }
+
+    void calDiv(){
+        double dividendAmount;
+            if(memberType.equals("Premium")){
+                dividendAmount=(sharesValue * 8)/100;
+            } else if (memberType.equals("Regular")) {
+                dividendAmount=(sharesValue * 5)/100;
+            }
+            else {
+                dividendAmount=0;
+            }
+        System.out.println("Dividend amount is " + dividendAmount);
+    }
+}
 public class Main{
     public static void main(String[] args) {
         savingAccount s1=new savingAccount("AAAA",12345678,52478,12);
@@ -80,17 +103,24 @@ public class Main{
         sharesAccount sh1=new sharesAccount("CCCC",987456321,45896,1500,154);
         sharesAccount sh2=new sharesAccount("DDDD",321456789,56875,567,100);
 
+        divCal d1=new divCal("EEEE",14785236,58547,"Premium",1547);
+        divCal d2=new divCal("FFFF",58796734,54782,"Regular",354);
+
         ArrayList<Account> accounts=new ArrayList<>();
         accounts.add(s1);
         accounts.add(s2);
         accounts.add(sh1);
         accounts.add(sh2);
+        accounts.add(d1);
+        accounts.add(d2);
 
         HashMap<Long, Account> accountMap=new HashMap<>();
         accountMap.put(s1.accNo, s1);
         accountMap.put(s2.accNo, s2);
         accountMap.put(sh1.accNo, sh1);
         accountMap.put(sh2.accNo, sh2);
+        accountMap.put(d1.accNo, d1);
+        accountMap.put(d2.accNo, d2);
 
         for(Account acc : accounts){
             acc.showDetails();
@@ -102,10 +132,22 @@ public class Main{
         long accountNo=sc.nextLong();
         Account select=accountMap.get(accountNo);
 
+        if(select == null){
+            System.out.println("Account not found , Enter valid account number.");
+            return;
+        }
+
         if(select instanceof sharesAccount){
             sharesAccount sa=(sharesAccount) select;
             sa.calShares();
             sa.showDetails();
+        }
+
+        if(select instanceof divCal){
+            divCal d=(divCal) select;
+            d.showDetails();
+            System.out.println("Member Type is " +d.memberType);
+            d.calDiv();
         }
 
         try {
