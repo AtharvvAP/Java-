03fc05598eecbbd1aabd5bf320a91c0f64bd6d09
Atharvvap <atharva.ap.04@gmail.com>
2026-08-05 14:39:40 +0530

    tried most used string methods

--- src/Bank/DsaProblems.java
+++ src/Bank/DsaProblems.java
@@ -1,20 +1,48 @@
 package Bank;
 
-import java.util.HashMap;
-
-public class DsaProblems {
+public  class DsaProblems{
     public static void main(String[] args) {
-        int arr[]={10,99,666,223,448};
-        int target=109;
+        String s = "Java Programming";
 
-        HashMap<Integer, Integer>seen=new HashMap<>();
+        System.out.println("Length of string = " + s.length());
 
-        for(int i=0; i<arr.length; i++){
-            int complement=target - arr[i];
-            if(seen.containsKey(complement)){
-                System.out.println("Found " + complement + " + " + arr[i] + " = " +target);
-            }
-            seen.put(arr[i], i);
+        System.out.println("Char at 8 = " + s.charAt(8));
+
+        System.out.println("substring 11 = " +s.substring(11));
+
+        System.out.println("Contains(\"Prog\") : " + s.contains("Prog"));
+
+        System.out.println("starts with (\" Ja\") : " + s.startsWith(" Ja"));
+
+        System.out.println("end with (\"ing \") : " + s.endsWith("ing "));
+
+        System.out.println("Equals to " + s.equals("Programming"));
+
+        System.out.println("equalsIgnoreCase (\"  java programming  \") : " + s.equalsIgnoreCase("java programming"));
+
+        System.out.println("index of (n) : " + s.indexOf('n'));
+
+        System.out.println("last index of (r) : " + s.lastIndexOf('r'));
+
+        System.out.println("replaced : " + s.replace("Java", "Core Java"));
+
+        System.out.println("replace all r with @ : " + s.replaceAll("r", "@"));
+
+        System.out.println("to upper case : " + s.toUpperCase());
+
+        System.out.println("to lower case : " + s.toLowerCase());
+
+        System.out.println("triming " + s.trim());
+
+        String[] words=s.trim().split(" ");
+        System.out.println("split() : ");
+        for(String word : words){
+            System.out.println(word);
         }
+
+        System.out.println("\"\".is Empty : " + "".isEmpty());
+
+        System.out.println("\"   \".is Blank :" +"  ".isBlank());
+
     }
 }
