f7e22697ec313fac9ab9977393796ee108ca6a39
Atharvvap <atharva.ap.04@gmail.com>
2026-08-19 14:44:43 +0530

    started dsa problems on string

--- src/Bank/DsaProblems.java
+++ src/Bank/DsaProblems.java
@@ -7,12 +7,25 @@ import java.util.Map;
 
 public  class DsaProblems{
     public static void main(String[] args) {
-        String s="Hello";
+//        String s="Hello";
+//        String reversed="";
+//
+//        for(int i=s.length()-1; i>=0; i--){
+//            reversed= reversed + s.charAt(i);
+//        }
+//        System.out.println(reversed);
+
+        String s="hello";
         String reversed="";
 
         for(int i=s.length()-1; i>=0; i--){
-            reversed= reversed + s.charAt(i);
+            reversed +=s.charAt(i);
         }
-        System.out.println(reversed);
+        if(s.equals(reversed)){
+            System.out.println("String is palindrome : " + reversed);
+        }
+        else {
+            System.out.println("String is not palindrome : " + reversed);
+        }
     }
 }
