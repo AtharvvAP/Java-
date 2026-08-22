3277af33ce768f78fcf81e7d1214c9e994a7efe1
Atharvvap <atharva.ap.04@gmail.com>
2026-08-22 16:09:17 +0530

    started dsa problems on string on finding largest prefix

--- src/Bank/DsaProblems.java
+++ src/Bank/DsaProblems.java
@@ -5,60 +5,31 @@ import java.util.*;
 public  class DsaProblems{
     public static void main(String[] args) {
 
-//        Scanner sc=new Scanner(System.in);
-//        System.out.print("Enter 1st string : ");
-//        String s1=sc.nextLine();
-//
-//        System.out.print("Enter 2nd string : ");
-//        String s2=sc.nextLine();
-//
-//        HashMap<Character, Integer> map1 =new HashMap<>();
-//        HashMap<Character, Integer> map2 =new HashMap<>();
-//
-//        for(int i=0; i<s1.length(); i++){
-//            char ch=s1.charAt(i);
-//            if(map1.containsKey(ch)){
-//                map1.put(ch, map1.get(ch)+1);
-//            }
-//            else {
-//                map1.put(ch, 1);
-//            }
-//        }
-//        for(int i=0; i< s2.length(); i++){
-//            char ch=s2.charAt(i);
-//            if (map2.containsKey(ch)) {
-//                map2.put(ch, map2.get(ch)+1);
-//            }
-//            else {
-//                map2.put(ch, 1);
-//            }
-//        }
-//        System.out.println(map1);
-//        System.out.println(map2);
-//
-//        System.out.println("--------------------------------");
-//
-//        if(map1.equals(map2)){
-//            System.out.println("It is valid anagram");
-//        }
-//        else {
-//            System.out.println("It is not anagram");
-//        }
-        HashMap<Character, Integer> map =new HashMap<>();
-        String s="wowww";
+        String strings[]={"flower", "flow", "flowchart"};
 
-            for(char c: s.toCharArray()){
-                if(map.containsKey(c)){
-                    map.put(c, map.get(c)+1);
-                }
-                else {
-                    map.put(c, 1);
+        String shortest=strings[0];
+
+
+        for(int i=1; i<strings.length; i++){
+            if(strings[i].length() < shortest.length()){
+                shortest=strings[i];
+            }
+        }
+        System.out.println(shortest);
+
+        System.out.println("--------------------------");
+
+        String result="";
+
+        for(int i=0; i<shortest.length(); i++){
+            for(int j=0; j<strings.length; j++){
+                if(shortest.charAt(i) != strings[j].charAt(i)){
+                    System.out.println("Longest common prefix is  " + result);
+                    return;
                 }
             }
-            for(char c: s.toCharArray()){
-                if(map.get(c)==1){
-                    System.out.println("First non-repeating character is : " + c );
-                }
-            }
+            result += shortest.charAt(i);
+        }
+        System.out.println("Longest common prefix is : " + result);
     }
 }
